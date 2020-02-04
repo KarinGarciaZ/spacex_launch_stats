5b5c6853dc67f2fe59f03c413e4f746aaa75d9ca
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2020-02-03 23:18:58 -0600

    Created server to run the project.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const graphHTTP = require("express-graphql")
+const schema = require('./graphql/schema')
+
+const app = express()
+
+app.use('/graphql', graphHTTP({
+  schema,
+  graphiql: true
+}))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`))
