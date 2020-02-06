28b3140ee354ae9c1b53ede6d32b6ff7d32527af
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2020-02-05 22:03:16 -0600

    Added cors to the server

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const graphHTTP = require("express-graphql")
 const schema = require('./graphql/schema')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 app.use('/graphql', graphHTTP({
   schema,
   graphiql: true
-app.use('/graphql', graphHTTP({
 }))
 
 const PORT = process.env.PORT || 5000
